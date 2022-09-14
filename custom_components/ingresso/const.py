d799368ea91d60ed12753932c468fa8057a38018
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2022-09-14 17:29:11 -0300

    Update const.py

--- custom_components/ingresso/const.py
+++ custom_components/ingresso/const.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 BASE_URL = "https://api-content.ingresso.com/v0/templates/nowplaying/{}?partnership={}"
 DEFAULT_POSTER = "https://www.promoview.com.br/uploads/2019/01/images/07.01.2019/ingresso.comlogo.jpg"
 ICON = "mdi:ticket"
@@ -5,3 +7,4 @@ ICON = "mdi:ticket"
 CONF_CITY_ID = "city_id"
 CONF_CITY_NAME = "city_name"
 CONF_PARTNERSHIP = "partnership"
+SCAN_INTERVAL = timedelta(minutes=120)
