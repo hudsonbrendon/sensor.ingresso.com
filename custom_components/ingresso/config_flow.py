b1b99588705b0c12e001cc39fdca32d8fef75202
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2023-04-26 03:10:06 -0300

    refactor: refact config flow

--- custom_components/ingresso/config_flow.py
+++ custom_components/ingresso/config_flow.py
@@ -1,10 +1,12 @@
+from collections.abc import Mapping
+
 import voluptuous as vol
 from homeassistant import config_entries
-from .const import CONF_CITY_ID, CONF_CITY_NAME, CONF_PARTNERSHIP
-from .const import DOMAIN
 
+from . import get_movies
+from .const import CONF_CITY_ID, CONF_CITY_NAME, CONF_PARTNERSHIP, DOMAIN
 
-DATA_SCHEMA = vol.Schema(
+DATA_SCHEMA: vol.Schema = vol.Schema(
     {
         vol.Required(CONF_CITY_ID): int,
         vol.Required(CONF_CITY_NAME): str,
@@ -14,21 +16,44 @@ DATA_SCHEMA = vol.Schema(
 
 
 class IngressoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Ingresso.com config flow."""
+    """Ingresso config flow."""
+
+    def __init__(self) -> None:
+        """Initialize Ingresso config flow."""
+        self.city_id: str
+        self.city_name: str
+        self.partnership: str
 
     async def async_step_user(self, user_input=None):
         errors = {}
 
         if user_input is not None:
-            return self.async_create_entry(
-                title=f"{user_input[CONF_PARTNERSHIP].capitalize()} {user_input[CONF_CITY_NAME].capitalize()}",
-                data={
-                    CONF_CITY_ID: user_input[CONF_CITY_ID],
-                    CONF_CITY_NAME: user_input[CONF_CITY_NAME],
-                    CONF_PARTNERSHIP: user_input[CONF_PARTNERSHIP],
-                },
-            )
+            if await get_movies(
+                hass=self.hass,
+                city_id=user_input.get(CONF_CITY_ID),
+                partnership=user_input.get(CONF_PARTNERSHIP),
+            ):
+                self.city_id = user_input.get(CONF_CITY_ID)
+                self.city_name = user_input.get(CONF_CITY_NAME)
+                self.partnership = user_input.get(CONF_PARTNERSHIP)
+
+                return self.async_create_entry(
+                    title=f"{self.partnership.capitalize()} {self.city_name.capitalize()}",
+                    data={
+                        CONF_CITY_ID: self.city_id,
+                        CONF_CITY_NAME: self.city_name,
+                        CONF_PARTNERSHIP: self.partnership,
+                    },
+                )
+
+            errors[CONF_CITY_ID] = "auth_error"
+            errors[CONF_PARTNERSHIP] = "auth_error"
 
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+            step_id="user",
+            data_schema=self.add_suggested_values_to_schema(
+                DATA_SCHEMA,
+                user_input,
+            ),
+            errors=errors,
         )
--- custom_components/ingresso/const.py
+++ custom_components/ingresso/const.py
@@ -1,6 +1,5 @@
 from datetime import timedelta
 
-
 BASE_URL = "https://api-content.ingresso.com/v0/templates/nowplaying/{}?partnership={}"
 DEFAULT_POSTER = "https://www.promoview.com.br/uploads/2019/01/images/07.01.2019/ingresso.comlogo.jpg"
 ICON = "mdi:ticket"
